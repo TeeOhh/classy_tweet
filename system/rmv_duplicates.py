21188b9f8688d4f23e52ac0390b10fd11392565d
TeeOhh <taylorolson50@yahoo.com>
2018-03-09 19:09:56 -0600

    finished interface

--- system/rmv_duplicates.py
+++ system/rmv_duplicates.py
@@ -1,8 +1,6 @@
 ##Removes duplicate tweets after pulling
 import pandas as pd
-import sys
 
-file_name = sys.argv[1]
-df = pd.read_csv(file_name)
+df = pd.read_csv('past_tweets.csv')
 df = df.drop_duplicates(subset='tweet_text', keep='first')
-df.to_csv(file_name, columns=['user', 'location', 'tweet_text', 'label'])
+df.to_csv('past_tweets.csv', columns=['user', 'location', 'tweet_text', 'label'])
